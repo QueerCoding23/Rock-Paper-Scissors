2eff6247ae696b40797c17ca98c1badf3de4ec3f
QueerCoding23 <71687109+QueerCoding23@users.noreply.github.com>
2020-09-22 23:50:54 +1000

    Uh Oh
    - Reformatted UI
    -Did some local side screw ups and fixed them uwu

---
+++ Rock Paper Scissors/main.py
@@ -0,0 +1,89 @@
+#Rock Paper Scissors!
+
+import time, random
+
+turn = 0
+pScore = 0
+cScore = 0
+
+
+def pause(number):
+    time.sleep(number)
+
+
+def text(string):
+    print(string)
+    pause(1)
+
+
+def computerChoice():
+    return random.randint(0, 2)
+
+
+def playerChoice():
+    choice = input("Rock, Paper, Scissors?\n"
+    ">>> ")
+    if choice.lower() == 'rock':
+        return 0
+    elif choice.lower() == 'paper':
+        return 1
+    elif choice.lower() == 'scissors':
+        return 2
+    else:
+        return 'end'
+
+
+def winCalc(player, computer):
+    if player == 0:
+        if computer == 0:
+            return 'draw'
+        elif computer == 1:
+            return 'loss'
+        else: 
+            return 'win'
+    elif player == 1:
+        if computer == 0:
+            return 'win'
+        elif computer == 1:
+            return 'draw'
+        else: return 'loss'
+    else:
+        if computer == 0:
+            return 'loss'
+        elif computer == 1:
+            return 'win'
+        else: 
+            return 'draw'
+
+
+while True:
+    text(f" Round: {turn}\n"
+    f"  Your Score: {pScore}\n"
+    f"  Computer Score: {cScore}")
+
+    pMove = playerChoice()
+    if pMove == 'end':
+        break
+    cMove = computerChoice()
+    playerState = winCalc(pMove, cMove)
+    if playerState == 'win':
+        pScore += 1
+        text("Win!")
+    elif playerState == 'loss':
+        cScore += 1
+        text("Loss...")
+    else:
+        text("Draw")
+    turn += 1
+
+if pScore > cScore:
+    winner = "Player"
+elif cScore > pScore:
+    winner = "Computer"
+else:
+    winner = "Draw"
+text(f"Total Rounds: {turn}\n"
+    f"Your final score: {pScore}\n"
+    f"Computer's final score: {cScore}\n"
+    f"Winner: {winner}")
+    
